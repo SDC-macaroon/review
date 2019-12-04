12150411b42afc9f9cc2a6413ffcf90054158aa2
Javed Bartlett <javedbartlett@gmail.com>
2019-12-03 19:44:31 -0800

    Project is setup

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
+import faker from 'faker';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello from App Component</h1>
+        <h1>Hello from App Component! Testing Faker!</h1>
+        <h2>{faker.name.findName()}</h2>
+        <h3>{faker.fake( "{{name.findName}} {{name.suffix}}" )}</h3>
+        <div>{faker.lorem.sentence()}</div>
+        <div>{faker.image.cats()}</div>
+        <img src={faker.image.cats()}></img>
       </div>
     );
   }
