939c32c29979457a7551c39451161709f80831d0
Javed Bartlett <javedbartlett@gmail.com>
2019-12-04 16:19:14 -0800

    fixed all airbnb eslint issues

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(__dirname + '/../client/dist'))
+app.use(express.static(`${__dirname}/../client/dist`));
 
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
