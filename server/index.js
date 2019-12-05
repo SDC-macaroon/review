5040a468ba767f99d2b470c96bc3ce5ad8165d7c
Javed Bartlett <javedbartlett@gmail.com>
2019-12-04 16:16:21 -0800

    Added airbnb Eslinter and edited code to satisfy linter.

--- server/index.js
+++ server/index.js
@@ -8,6 +8,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(__dirname + '/../client/dist'))
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
