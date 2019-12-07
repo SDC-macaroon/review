1bcbcfb1374be3c773e31c571ee41db571b6ddba
Javed Bartlett <javedbartlett@gmail.com>
2019-12-07 14:04:58 -0800

    uppercase review for 1 star rating

--- client/src/components/ReactStrapTest.jsx
+++ client/src/components/ReactStrapTest.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable  */
 import React, { useState } from 'react';
 import StarRatingComponent from 'react-star-rating-component';
 import moment from 'moment';
@@ -20,21 +21,40 @@ const ReactStrapTest = props => {
   return (
     <div>
       <div className="reviewButton" onClick={toggle}>
-        + Read All {reviews.length} Reviews</div>
-      <Modal isOpen={modal} toggle={toggle} className={className} external={externalCloseBtn}>
+        + Read All {reviews.length} Reviews
+      </div>
+      <Modal
+        isOpen={modal}
+        toggle={toggle}
+        className={className}
+        external={externalCloseBtn}
+      >
         <ModalBody>
           <div className="reviewsModalTitle">Reviews</div>
           {reviews.map(review => (
             <div key={review._id}>
-              <StarRatingComponent className="stars" name="rate1" starCount={review.rating} editing={false} value={5} />
-              <div className="reviewTitle">{review.reviewTitle.charAt(0).toUpperCase() + review.reviewTitle.slice(1)}</div>
-              <div className="reviewAuthor">by {review.reviewAuthor} on {moment(review.reviewDate).format('LL')}</div>
+              <StarRatingComponent
+                className="stars"
+                name="rate1"
+                starCount={review.rating}
+                editing={false}
+                value={5}
+              />
+              <div className="reviewTitle">
+                {review.reviewTitle.charAt(0).toUpperCase() +
+                  review.reviewTitle.slice(1)}
+              </div>
+              <div className="reviewAuthor">
+                by {review.reviewAuthor} on{' '}
+                {moment(review.reviewDate).format('LL')}
+              </div>
               <div className="reviewBody">
-                { review.reviewBody }
+                {review.rating === 1
+                  ? review.reviewBody.toUpperCase()
+                  : review.reviewBody}
               </div>
             </div>
           ))}
-
         </ModalBody>
       </Modal>
     </div>
