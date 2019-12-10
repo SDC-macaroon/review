2fea70d9165d014539c8ad942757b485f5a0ecdd
Javed Bartlett <javedbartlett@gmail.com>
2019-12-09 23:39:29 -0800

    docker test deploy

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoUri = 'mongodb://localhost/reviewDB2';
+const mongoUri = 'mongodb://mongo:27017/mongo-test';
 const db = mongoose.connect(mongoUri, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
