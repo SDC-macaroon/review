e292e1be1919b1900602ce68561af42042c8d156
Javed Bartlett <javedbartlett@gmail.com>
2019-12-14 13:09:55 -0800

    removed element level css

--- client/src/components/Features.jsx
+++ client/src/components/Features.jsx
@@ -6,14 +6,14 @@ const Features = () => (
     <br />
     <br />
     <ul className="featuresList">
-      <li>Classic, generous, boxy fit</li>
-      <li>
+      <li className="jli jli1">Classic, generous, boxy fit</li>
+      <li className="jli">
         Solid colors are 100% preshrunk cotton, heather grey is 90% cotton, 10%
         polyester, denim heather is 50% cotton, 50% polyester
       </li>
-      <li>Fabric weight: 5.3 oz (180gsm) heavyweight</li>
-      <li>Double-needle hems and neck band for durability</li>
-      <li>Ethically sourced</li>
+      <li className="jli">Fabric weight: 5.3 oz (180gsm) heavyweight</li>
+      <li className="jli">Double-needle hems and neck band for durability</li>
+      <li className="jli">Ethically sourced</li>
     </ul>
   </div>
 );
