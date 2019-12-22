3b4b11970b23a3635a9ca7f2bf382f1728e054ec
ptbarnum4 <peterbarnum@gmail.com>
2019-12-21 22:08:31 -0700

    Rebase with master
    cleaned up this file a bit to seperate each endpoint a little more clearly.
    
    Complete:
    Reviews:
      Create/POST
        Endpoint  |  /reviews/<productID>
        Use:      |  Create one product review
        Response: |  Updated list of reviews for current product
      Read/GET
        Endpoint  |  /reviews/<productID>
        Use:      |  Retrieve all reviews for product with <productID>
        Response: |  All reviews for product
    
    Review:
      Create/POST
        Endpoint  |  /review/<productID>
        Use:      |  Create one product review.
        Response: |  Successfully stored review
      Read/GET
        Endpoint  |  /review/<productID>/<reviewID>
        Use:      |  Retrieve one review for <productID> with <reviewID>
        Response: |  One review with <revieweID>
      Update/PUT
        Endpoint  |  /reviews/<productID>/<reviewID>
        Use:      |  Update review
        Response: |  Updated list of reviews for current product
      Delete/DELETE
        Endpoint  |  /reviews/<productID>/<reviewID>
        Use:      |  Delete review <reviewID> for product with <productID>
        Response: |  Send deleted product (like Array.pop() it will give one last opportunity to use it)
    
    Unfinished:
    Products:
      Create/POST
      Read/GET
      Update/PUT
      Delete/DELETE

--- server/index.js
+++ server/index.js
@@ -1,142 +1,118 @@
+/* eslint-disable */
 const express = require('express');
 const faker = require('faker');
-
 const _ = require('lodash');
-
 const bodyParser = require('body-parser');
 const cors = require('cors');
-// const { ProductModel, ReviewModel } = require('../database/Reviews.js'); // eslint-disable-line
 const db = require('./db/og-mongo/queries.js');
 
+const port = process.env.PORT || 3002;
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use('/product/:productID', express.static(`${__dirname}/../public`));
 
+/************************************
+***             REVIEWS           ***
+*************************************/
+/*
+  Create One Product Review
+*/
+app.post('/reviews/:productID', async (req, res) => {
+  const reviews = await db.reviews.create(req.params, req.body);
+  res.status(200).send(reviews);
+});
 
-/* Fetch All Reviews for a Product */
+/*
+  Read All Reviews for a Product
+*/
 app.get('/reviews/:productID', async (req, res) => {
   const product = await db.reviews.read(req.params);
   res.status(200).send(product.reviews);
 });
 
-/* Create One Product Review */
-// ! Still needs to accept user review
-app.post('/reviews/:productID', async (req, res) => {
-  const reviews = await db.reviews.create(req.params, req.body);
-  res.status(200).send(reviews);
-});
+/************************************
+***             REVIEW            ***
+*************************************/
 
-/* Fetch One Product Review */
-app.get('/review/:productID/:reviewID', async (req, res) => {
-  const review = await db.review.read(req.params);
-  res.status(200).send(review);
-});
-
-/* Create One Product Review */
+/*
+  Create One Product Review
+*/
 app.post('/review/:productID', async (req, res) => {
   const reviews = await db.reviews.create(req.params, req.body);
   res.status(200).send(reviews);
 });
 
-/* Update One Product Review */
-// eslint-disable-next-line no-unused-vars
+/*
+  Read One Product Review
+*/
+app.get('/review/:productID/:reviewID', async (req, res) => {
+  const review = await db.review.read(req.params);
+  res.status(200).send(review);
+});
+
+/*
+  Update One Product Review
+*/
 app.put('/review/:productID/:reviewID', async (req, res) => {
   const reviews = await db.review.update(req.params, req.body);
   res.status(200).send(reviews);
 });
 
-// TODO *********************
-
-/* Delete One Product Review */
-// eslint-disable-next-line no-unused-vars
+/*
+  TODO: Delete One Product Review
+*/
 app.delete('/review/:productID/:reviewID', async (req, res) => {
   const reviews = await db.review.delete(req.params);
   res.status(200).send(reviews);
 });
-// TODO Products
-/* Create One Product */
-// eslint-disable-next-line no-unused-vars
-app.post('/product/:productID', async (req, res) => {
-  //! Accepts data from request
+
+/************************************
+* TODO:        PRODUCTS           ***
+*************************************/
+
+/*
+TODO:  Create One Product
+*/
+app.post('/products/:productID', async (req, res) => {
+  // CREATE ONE PRODUCT
+    // ACCEPTS PRODUCT ID
 });
 
-/* Fetch One Product */
-// eslint-disable-next-line no-unused-vars
-app.get('/product/:productID', async (req, res) => {
-  // fetch entire product
-=======
-  const product = await ProductModel
-    .find({ productID: req.params.productID })
-    .exec();
-
-  res.status(200).send(product[0].reviews);
+/*
+TODO:  Read One Product
+*/
+app.get('/products/:productID', async (req, res) => {
+  //  READ ONE PRODUCT
+    //  ACCEPTS PRODUCT ID
 });
 
-
-
-
-// Create
-app.post('/reviews/:productID', async (req, res) => {
-  const { productID } = req.params;
-  let reviewID = await ProductModel
-    .find({ productID });
-  reviewID = reviewID[0].reviews.length;
-  console.log(reviewID);
-
-  const review = new ReviewModel({
-    reviewID,
-    rating: 4,
-    reviewTitle: 'THE BEST REVIEW',
-    reviewBody: faker.lorem.paragraph(),
-    reviewAuthor: 'Peter Barnum',
-    reviewDate: new Date(),
-  });
-
-<<<<<<< HEAD
-  await ProductModel.updateOne({ productID }, { $push: { reviews: review } },
-    (err, results) => {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(results);
-      }
-    });
-  res.status(200).send(review);
->>>>>>> Post works
-});
-
-/* Update One Product */
-// eslint-disable-next-line no-unused-vars
+/*
+TODO:  Update One Product
+*/
 app.put('/products/:productID', async (req, res) => {
-  // Update one product
-=======
-  const product = await ProductModel
-    .findOne({ productID });
-  product.reviews.push(review);
-  await product.save();
-
-  res.status(200).send(product);
->>>>>>> POST works, needs to verify author to prevent multiple reviews
+  //  UPDATE ONE PRODUCT
+    //  ACCEPTS PRODUCT ID
 });
 
-
-/* Delete One Product  */
-// eslint-disable-next-line no-unused-vars
+/*
+TODO:  Delete One Product
+*/
 app.delete('/products/:productID', async (req, res) => {
-  // Delete one product
+  //  DELETE ONE PRODUCT
+    //  ACCEPTS PRODUCT ID
 });
 
 
-// OPTIONS
+/************************************
+* TODO:        OPTIONS            ***
+*************************************/
 /* Delete One Product Review */
-// eslint-disable-next-line no-unused-vars
 app.options('/reviews', async (req, res) => {
 });
 
-const port = process.env.PORT || 3002;
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
-
 module.exports = app;
